9808bee1910afec1305a33ce95ea0a1a1ea42782
bfnsga <bfnsga@gmail.com>
2020-04-28 15:46:56 -0400

    Create .eleventy.js

---
+++ .eleventy.js
@@ -0,0 +1,44 @@
+const {
+    DateTime
+} = require("luxon");
+const htmlmin = require("html-minifier");
+
+module.exports = function(eleventyConfig) {
+    // Folders to copy to build dir (See. 1.1)
+    eleventyConfig.addPassthroughCopy("src/static");
+    eleventyConfig.addPassthroughCopy("src/files");
+
+  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+    if( outputPath.endsWith(".html") ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      });
+      return minified;
+    }
+
+    return content;
+  });
+
+// https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+        return DateTime.fromJSDate(dateObj, {
+            zone: 'utc'
+        }).toFormat('yyyy-LL-dd');
+    });
+
+return {
+        dir: {
+            input: "src/",
+            output: "dist",
+            includes: "_includes",
+            layouts: "_layouts"
+        },
+        templateFormats: ["html", "md", "njk"],
+        htmlTemplateEngine: "njk",
+
+        // 1.1 Enable eleventy to pass dirs specified above
+        passthroughFileCopy: true
+    };
+};
