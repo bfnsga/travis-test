576e9b224fe55eae154cbabf46258129ec8e01d0
bfnsga <bfnsga@gmail.com>
2020-04-28 19:50:46 -0400

    Update tailwind.config.js
    Need to simplify to get to work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,1 @@
-// tailwind.config.js
-const defaultTheme = require('tailwindcss/defaultTheme')
 
-module.exports = {
-  theme: {
-    extend: {
-      fontFamily: {
-        display: ['Space Mono', 'sans-serif'],
-      },
-    },
-  },
-}
