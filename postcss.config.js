30501e8f8f9becd52cb37e341d6a64dd1e5d1eef
bfnsga <bfnsga@gmail.com>
2020-04-29 08:17:28 -0400

    Update postcss.config.js
    Updated back to old style now that travis-ci seems to be working

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,17 @@
-{
+const purgecss = require('@fullhuman/postcss-purgecss')
+const cssnano = require('cssnano')
+
 module.exports = {
   plugins: [
     require('tailwindcss'),
+    require('autoprefixer'),
+    purgecss({
+      content: ['dist/**/*.html'],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    }),
+    cssnano({
+            preset: 'default',
+        })
     require('autoprefixer')
   ]
-}
 }
