d3e6ea6542df0dc8a4c184eec416b8bf9de7438d
bfnsga <bfnsga@gmail.com>
2020-04-28 20:16:53 -0400

    Update postcss.config.js
    Removed options for testing

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,6 @@
-const purgecss = require('@fullhuman/postcss-purgecss')
-const cssnano = require('cssnano')
-
 module.exports = {
   plugins: [
     require('tailwindcss'),
-    require('autoprefixer'),
-    purgecss({
-      content: ['dist/**/*.html'],
-      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-    }),
-    cssnano({
-            preset: 'default',
-        })
+    require('autoprefixer')
   ]
 }
